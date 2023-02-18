9cdb960ccd63ae4b160d2cd4b4f39523bb1c596b
PHIL KIM <philsgu@gmail.com>
2023-02-18 13:36:55 -0800

    reverted image location

--- stream_app.py
+++ stream_app.py
@@ -17,7 +17,7 @@ st.header("Geolocate ERAS Applicants")
 st.write(f"Last update: {formatted_date} [Phillip Kim, MD, MPH](https://www.doximity.com/pub/phillip-kim-md-8dccc4e4)")
 st.write("Upload raw CSV file from ERAS download to view map and output to HTML file. No data is saved on the server for privacy protection. Any resulting HTML file chosen to be saved locally will be at the program's discretion.")
 
-st.image("philsgu/geolocate/sample_geo.jpg")
+st.image("sample_geo.jpg")
 eras = "https://auth.aamc.org/account/#/login?gotoUrl=http:%2F%2Fpdws.aamc.org%2Feras-pdws-web%2F"
 
 st.markdown(f"### HOW TO INSTRUCTIONS\n1. Login into [PDWS ERAS]({eras})\n2. Select **Active Applicants**\n3. Select All top table column\n4. Action to perform on selected applicants: CSV Export\n5. Edit CSV Export\n6. Add a new export template\n7. Create a **CSV Export Name**\n8. Personal >> Select\n   - **AAMC ID (required)**\n   - **Applicant Name (required)**\n   - **Permanent Address (required)**\n9. Education >> Select\n   - **Medical School of Graduation (required)**\n    - **Medical School Country**\n   - **Medical School Degree Date of Graduation**\n10. Exams/Licsenses/Certifications >> Select\n    - **ALL COMLEX AND STEP SCORES**\n11. Select **Bulk Print Requests** AND save your Print Job Name to your computer")
